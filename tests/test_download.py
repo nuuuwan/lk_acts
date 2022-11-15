3d44683dc66f4f2dee30a72c599240d868f74a69
nuuuwan <nuuuwan@gmail.com>
2022-11-15 10:43:54 +0530

    refac: Changed filenames

--- tests/test_download.py
+++ tests/test_download.py
@@ -7,15 +7,15 @@ from lk_acts import download
 class TestCase(unittest.TestCase):
     def testGetFile(self):
         self.assertEqual(
-            download.get_file('bill-99-personal-data-protection'),
-            'docs/bill-99-personal-data-protection.pdf',
+            download.get_file('bill-2022-09-personal-data-protection'),
+            'data/bill-2022-09-personal-data-protection.pdf',
         )
 
     def testDownloadBill(self):
         for [url, label] in [
             [
                 'http://documents.gov.lk/files/bill/2022/1/167-2022_E.pdf',
-                'bill-99-personal-data-protection',
+                'bill-2022-09-personal-data-protection',
             ]
         ]:
             doc_file = download.get_file(label)
