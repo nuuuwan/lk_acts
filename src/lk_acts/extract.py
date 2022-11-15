41ab6fbe35760c20fedcc11f2ae5f94fe0a1f6cc
nuuuwan <nuuuwan@gmail.com>
2022-11-15 15:34:35 +0530

    feat: extract

---
+++ src/lk_acts/extract.py
@@ -0,0 +1,52 @@
+import re
+
+REGEX_CLAUSE = '(?P<clause_num>\\d+)\\.'
+REGEX_SUBCLAUSE = '\\((?P<subclause_num>\\d+)\\)\\s.*'
+REGEX_PARAGRAPH = '\\((?P<paragraph_num>[ivx]+)\\)\\s.*'
+REGEX_SUB_PARAGRAPH = '\\((?P<sub_paragraph_num>[a-z])\\)\\s.*'
+
+
+def extract_data(textlines):
+    data = []
+    clause_num = None
+    subclause_num = None
+    paragraph_num = None
+    sub_paragraph_num = None
+    for textline in textlines:
+        stripped_text = textline['text'].strip()
+        if textline['class_name'] == 'normal-bold':
+            result = re.match(REGEX_CLAUSE, stripped_text)
+            if result:
+                clause_num = (int)(result.groupdict()['clause_num'])
+                subclause_num = None
+                paragraph_num = None
+                sub_paragraph_num = None
+
+        if textline['class_name'] == 'normal':
+            result = re.match(REGEX_SUBCLAUSE, stripped_text)
+            if result:
+                subclause_num = (int)(result.groupdict()['subclause_num'])
+                paragraph_num = None
+                sub_paragraph_num = None
+
+        if textline['class_name'] == 'normal':
+            result = re.match(REGEX_PARAGRAPH, stripped_text)
+            if result:
+                paragraph_num = result.groupdict()['paragraph_num']
+                sub_paragraph_num = None
+
+        if textline['class_name'] == 'normal':
+            result = re.match(REGEX_SUB_PARAGRAPH, stripped_text)
+            if result:
+                sub_paragraph_num = result.groupdict()['sub_paragraph_num']
+
+        data.append(
+            textline
+            | dict(
+                clause_num=clause_num,
+                subclause_num=subclause_num,
+                paragraph_num=paragraph_num,
+                sub_paragraph_num=sub_paragraph_num,
+            )
+        )
+    return data
