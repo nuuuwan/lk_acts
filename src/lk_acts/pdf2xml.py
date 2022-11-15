8bcfbf7c391e6887ae75087b03d4b02c50b68c6b
nuuuwan <nuuuwan@gmail.com>
2022-11-15 10:48:38 +0530

    feat: Support converting all pages

--- src/lk_acts/pdf2xml.py
+++ src/lk_acts/pdf2xml.py
@@ -6,8 +6,15 @@ PY_BIN = 'python3'
 BIN = '/Library/Frameworks/Python.framework/Versions/3.10/bin/pdf2txt.py'
 
 
-def convert(pdf_file, page_no, xml_file):
-    cmd = f'{PY_BIN} {BIN} -o {xml_file} -p {page_no} {pdf_file} '
+def convert(pdf_file, page_nos, xml_file):
+    cmd = ' '.join(
+        [
+            f'{PY_BIN} {BIN}',
+            f'-o {xml_file}',
+            f'-p {page_nos}' if page_nos != 'all' else '',
+            pdf_file,
+        ]
+    )
     log.debug(cmd)
     os.system(cmd)
-    log.info(f'Converted {pdf_file} (page {page_no}) into {xml_file}')
+    log.info(f'Converted {pdf_file} (page {page_nos}) into {xml_file}')
--- tests/test_pdf2xml.py
+++ tests/test_pdf2xml.py
@@ -7,11 +7,11 @@ from lk_acts import pdf2xml
 class TestCase(unittest.TestCase):
     def testConvert(self):
         pdf_file = 'data/bill-2022-09-personal-data-protection.pdf'
-        page_no = 1
-        xml_file = f'{pdf_file}.{page_no:06d}.xml'
+        page_nos = 'all'
+        xml_file = f'{pdf_file}.{page_nos}.xml'
         if os.path.exists(xml_file):
             os.remove(xml_file)
-        pdf2xml.convert(pdf_file, page_no, xml_file)
+        pdf2xml.convert(pdf_file, page_nos, xml_file)
         # self.assertTrue(os.path.exists(xml_file))
 
 
