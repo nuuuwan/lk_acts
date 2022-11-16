42e3e831aa35fd984a6d33b1c9f9d9a6dfae49c1
nuuuwan <nuuuwan@gmail.com>
2022-11-16 08:51:15 +0530

    refac: -> url2pdf (for consistency)

--- src/lk_acts/url2pdf.py
+++ src/lk_acts/url2pdf.py
@@ -4,14 +4,14 @@ from lk_acts._utils import get_file_name, log
 from lk_acts.METADATA_LIST import METADATA_LIST
 
 
-def download_bill(url, pdf_file):
+def convert(url, pdf_file):
     www.download_binary(url, pdf_file)
     log.info(f'Downloaded {url} to {pdf_file}')
 
 
 if __name__ == '__main__':
     config = METADATA_LIST[0]
-    download_bill(
+    convert(
         url=config['url'],
         pdf_file=get_file_name(config, 'pdf'),
     )
