35d5674340c8b9ecd94b9cb345b650dbca32cb05
nuuuwan <nuuuwan@gmail.com>
2022-11-16 08:53:20 +0530

    lint

--- src/lk_acts/url2pdf.py
+++ src/lk_acts/url2pdf.py
@@ -2,6 +2,7 @@ from utils import www
 
 from lk_acts._utils import log
 
+
 def convert(url, pdf_file):
     www.download_binary(url, pdf_file)
     log.info(f'Downloaded {url} to {pdf_file}')
@@ -10,6 +11,7 @@ def convert(url, pdf_file):
 if __name__ == '__main__':
     from lk_acts._utils import get_file_name
     from lk_acts.METADATA_LIST import METADATA_LIST
+
     config = METADATA_LIST[0]
     convert(
         url=config['url'],
