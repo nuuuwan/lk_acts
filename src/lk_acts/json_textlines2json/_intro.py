5618823d272afc294aeda9e3ad4816c766c4ce38
nuuuwan <nuuuwan@gmail.com>
2022-11-16 10:17:52 +0530

    feat: Added price and postage

--- src/lk_acts/json_textlines2json/_intro.py
+++ src/lk_acts/json_textlines2json/_intro.py
@@ -6,29 +6,52 @@ REGEX_PUBLISHED = (
     r'\(Published in the Gazette on'
     + r' (?P<month>\w+) (?P<day>\d+), (?P<year>\d{4})\)'
 )
+REGEX_PRICE = r'Price : Rs. (?P<price_lkr>\d+).00'
+REGEX_POSTAGE = r'Postage : Rs. (?P<postage_lkr>\d+).00'
 
 
 def extract_intro_data(textlines_with_metadata):
     short_title = ''
     long_title_lines = []
     date_published = None
+    price_lkr = None
+    postage_lkr = None
 
     for textline in textlines_with_metadata:
+        section_num = textline['section_num']
+        if section_num:
+            break
         text = clean_textline(textline['text'])
         font_size = (float)(textline['font_size'])
-        section_num = textline['section_num']
-        if not section_num:
-            result = re.match(REGEX_PUBLISHED, text)
-            if result:
-                date_published = result.groupdict()
-            elif 16 < font_size < 17:
-                short_title = text.title()
-            elif 11 < font_size < 14 and not date_published:
-                long_title_lines.append(text)
+
+        result = re.match(REGEX_PUBLISHED, text)
+        if result:
+            date_published = result.groupdict()
+            continue
+
+        result = re.match(REGEX_PRICE, text)
+        if result:
+            price_lkr = (int)(result.groupdict()['price_lkr'])
+            continue
+
+        result = re.match(REGEX_POSTAGE, text)
+        if result:
+            postage_lkr = (int)(result.groupdict()['postage_lkr'])
+            continue
+
+        if 16 < font_size < 17:
+            short_title = text.title()
+            continue
+
+        if 11 < font_size < 14 and not date_published:
+            long_title_lines.append(text)
+            continue
 
     long_title = join_textlines(long_title_lines)
     return dict(
         short_title=short_title,
         long_title=long_title,
         date_published=date_published,
+        price_lkr=price_lkr,
+        postage_lkr=postage_lkr,
     )
