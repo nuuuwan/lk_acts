efc9e40f1d913c64bb48b209f6b934fa59e6f929
nuuuwan <nuuuwan@gmail.com>
2022-11-16 10:19:35 +0530

    refac: reordered functions

--- src/lk_acts/json_textlines2json/_intro.py
+++ src/lk_acts/json_textlines2json/_intro.py
@@ -24,9 +24,17 @@ def extract_intro_data(textlines_with_metadata):
         text = clean_textline(textline['text'])
         font_size = (float)(textline['font_size'])
 
+        if 16 < font_size < 17:
+            short_title = text.title()
+            continue
+
         result = re.match(REGEX_PUBLISHED, text)
         if result:
             date_published = result.groupdict()
+            continue
+
+        if 11 < font_size < 14 and not date_published:
+            long_title_lines.append(text)
             continue
 
         result = re.match(REGEX_PRICE, text)
@@ -37,14 +45,6 @@ def extract_intro_data(textlines_with_metadata):
         result = re.match(REGEX_POSTAGE, text)
         if result:
             postage_lkr = (int)(result.groupdict()['postage_lkr'])
-            continue
-
-        if 16 < font_size < 17:
-            short_title = text.title()
-            continue
-
-        if 11 < font_size < 14 and not date_published:
-            long_title_lines.append(text)
             continue
 
     long_title = join_textlines(long_title_lines)
