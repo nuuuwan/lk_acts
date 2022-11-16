4b8fdf17292363e3f131286bd1fbfe776ab481f6
nuuuwan <nuuuwan@gmail.com>
2022-11-16 08:53:10 +0530

    refac: moved get_file_name import into main

--- src/lk_acts/url2pdf.py
+++ src/lk_acts/url2pdf.py
@@ -1,6 +1,6 @@
 from utils import www
 
-from lk_acts._utils import get_file_name, log
+from lk_acts._utils import log
 
 def convert(url, pdf_file):
     www.download_binary(url, pdf_file)
@@ -8,6 +8,7 @@ def convert(url, pdf_file):
 
 
 if __name__ == '__main__':
+    from lk_acts._utils import get_file_name
     from lk_acts.METADATA_LIST import METADATA_LIST
     config = METADATA_LIST[0]
     convert(
