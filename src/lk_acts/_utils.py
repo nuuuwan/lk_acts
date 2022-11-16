09f1c28a914f89101b6b29d36bf831b7991c64d0
nuuuwan <nuuuwan@gmail.com>
2022-11-16 08:50:22 +0530

    feat: Added get_dir_name, get_file_name

--- src/lk_acts/_utils.py
+++ src/lk_acts/_utils.py
@@ -1,3 +1,29 @@
-from utils import logx
+import os
+
+from utils import dt, logx
 
 log = logx.get_logger('lk_acts')
+
+
+def get_dir_name(config):
+    year = config['year']
+    num = config['num']
+    name = config['name']
+
+    name_kebab = dt.to_kebab(name)
+
+    return os.path.join(
+        'data',
+        f'{year}-{num:04d}-{name_kebab}',
+    )
+
+
+def get_file_name(config, ext):
+    dir_config = get_dir_name(config)
+    if not os.path.exists(dir_config):
+        os.mkdir(dir_config)
+
+    return os.path.join(
+        dir_config,
+        f'data.{ext}',
+    )
