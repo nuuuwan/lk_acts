7254c8578dffbdc9c008c8e88e067cf8f6901b77
nuuuwan <nuuuwan@gmail.com>
2022-11-16 08:49:50 +0530

    feat: Added METADATA_LIST

---
+++ src/lk_acts/METADATA_LIST.py
@@ -0,0 +1,8 @@
+METADATA_LIST = [
+    dict(
+        year=2022,
+        num=9,
+        name="Personal Data Protection",
+        url="http://documents.gov.lk/files/bill/2022/1/167-2022_E.pdf",
+    )
+]
