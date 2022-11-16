c48790e4dc4ff20868dc831b085e000cf5b4f87b
nuuuwan <nuuuwan@gmail.com>
2022-11-16 10:00:58 +0530

    feat: extract date published

--- src/lk_acts/json_textlines2json.py
+++ src/lk_acts/json_textlines2json.py
@@ -8,6 +8,10 @@ REGEX_SECTION = r'(?P<section_num>\d+)\.'
 REGEX_SUBSECTION = r'\((?P<subsection_num>\d+)\).*'
 REGEX_PARAGRAPH = r'\((?P<paragraph_num>[a-z])\).*'
 REGEX_SUB_PARAGRAPH = r'\((?P<sub_paragraph_num>[ivx]+)\).*'
+REGEX_PUBLISHED = (
+    r'\(Published in the Gazette on'
+    + r' (?P<month>\w+) (?P<day>\d+), (?P<year>\d{4})\)'
+)
 
 
 def clean_textline(s):
@@ -90,21 +94,27 @@ def add_metadata(textlines_original):
 def extract_intro_data(textlines_with_metadata):
     short_title = ''
     long_title_lines = []
-    is_beyond_published = False
+    date_published = None
+
     for textline in textlines_with_metadata:
         text = clean_textline(textline['text'])
         font_size = (float)(textline['font_size'])
         section_num = textline['section_num']
         if not section_num:
-            if text[:10] == '(Published':
-                is_beyond_published = True
+            result = re.match(REGEX_PUBLISHED, text)
+            if result:
+                date_published = result.groupdict()
             elif 16 < font_size < 17:
                 short_title = text.title()
-            elif 11 < font_size < 14 and not is_beyond_published:
+            elif 11 < font_size < 14 and not date_published:
                 long_title_lines.append(text)
 
     long_title = join_textlines(long_title_lines)
-    return dict(short_title=short_title, long_title=long_title)
+    return dict(
+        short_title=short_title,
+        long_title=long_title,
+        date_published=date_published,
+    )
 
 
 def extract_sections(textlines_with_metadata):
