da233133261b2a8dd186f3b01039810e6091dbb9
nuuuwan <nuuuwan@gmail.com>
2022-11-16 08:55:02 +0530

    refac: only pass config to convert

--- src/lk_acts/url2pdf.py
+++ src/lk_acts/url2pdf.py
@@ -1,19 +1,17 @@
 from utils import www
 
-from lk_acts._utils import log
+from lk_acts._utils import get_file_name, log
 
 
-def convert(url, pdf_file):
+def convert(config):
+    url = config['url']
+    pdf_file = get_file_name(config, 'pdf')
     www.download_binary(url, pdf_file)
     log.info(f'Downloaded {url} to {pdf_file}')
 
 
 if __name__ == '__main__':
-    from lk_acts._utils import get_file_name
     from lk_acts.METADATA_LIST import METADATA_LIST
 
     config = METADATA_LIST[0]
-    convert(
-        url=config['url'],
-        pdf_file=get_file_name(config, 'pdf'),
-    )
+    convert(config)
