7011992252f170ee20f817f266e229860891b3e8
nuuuwan <nuuuwan@gmail.com>
2022-11-20 10:50:16 +0530

    feat: Added merging logic

--- src/lk_acts/json_textlines2json/_merge_utils.py
+++ src/lk_acts/json_textlines2json/_merge_utils.py
@@ -1,10 +1,26 @@
-from lk_acts._utils import clean_textline
+from lk_acts._utils import clean_textline, join_textlines
+
+MIN_GAP = 10
 
 
 def merge_textlines(textlines):
-    return list(
-        map(
-            lambda textline: clean_textline(textline['text']),
-            textlines,
-        )
-    )
+    grouped_texts = []
+    current_text_lines = []
+    prev_y1 = None
+    for textline in textlines:
+        bbox = textline['bbox']
+        y1 = (float)(bbox['y1'])
+        y2 = (float)(bbox['y2'])
+        text = clean_textline(textline['text'])
+
+        if all([prev_y1 and prev_y1 - y2 > MIN_GAP]):
+            grouped_texts.append(join_textlines(current_text_lines))
+            current_text_lines = []
+        current_text_lines.append(text)
+
+        prev_y1 = y1
+
+    if current_text_lines:
+        grouped_texts.append(join_textlines(current_text_lines))
+
+    return grouped_texts
